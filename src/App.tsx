cccba1c6a1659a47a9ccff81946a5ea9cbb5c921
vivek sharma <vivek12sharma18@gmail.com>
2026-02-02 23:50:11 +0530

    checking if user is sign in or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import {getDatabase , ref , set } from "firebase/database";
-// import {app} from  "./firebase.tsx";
+import {app} from  "./firebase.tsx";
 import SignUpPage from "./Pages/signUp.tsx"
 import SignInPage from "./Pages/signIn.tsx";
 import './App.css';
@@ -8,6 +8,10 @@ import GoogleSignIn from "./Pages/googleSignIn.tsx";
 //instance for realtimedatabase
 //const db = getDatabase(app);
 
+import { getAuth,onAuthStateChanged,signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
+
+const auth = getAuth(app);
 
 function App() {
   // const sendData = ()=>{
@@ -17,7 +21,23 @@ function App() {
   //     height : '5.8'
   //   })
   // };
-  return (
+
+  const [isloggedin, setIsLoggedIn] = useState(false);
+  const [user,setUser] = useState<any>();
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,(user)=>{
+      if(user){
+        setIsLoggedIn(true);
+        setUser(user);
+      }
+      else{
+        setIsLoggedIn(false)
+      }
+    })
+  },[])
+  if(!isloggedin){
+    return (
     <>
     <h1>Firebase Learning </h1>
     {/* <button onClick={sendData}>sendData</button> */}
@@ -29,6 +49,16 @@ function App() {
     <GoogleSignIn></GoogleSignIn>
     </>
   )
+  }
+  else{
+   return(
+    <div>
+      <h1>Welcome User of this email Id : {user && user.email}</h1>
+      <button onClick={()=>{signOut(auth)}}>Sign Out</button>
+    </div>
+   ) 
+  }
+
 }
 
 export default App
