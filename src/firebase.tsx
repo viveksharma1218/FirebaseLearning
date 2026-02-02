fec0c14441a24302deb25bd39dd51e5c4d23157f
vivek sharma <vivek12sharma18@gmail.com>
2026-02-02 14:19:16 +0530

    react project create for learning Firebase.

---
+++ src/firebase.tsx
@@ -0,0 +1,13 @@
+import {initializeApp}  from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnqWkNynguLfZoctjC5BQ-V4a2VULycpw",
+  authDomain: "app-8ea54.firebaseapp.com",
+  projectId: "app-8ea54",
+  storageBucket: "app-8ea54.firebasestorage.app",
+  messagingSenderId: "914342466661",
+  appId: "1:914342466661:web:067ef1855df9c5690da28c"
+};
+
+const app = initializeApp(firebaseConfig);
